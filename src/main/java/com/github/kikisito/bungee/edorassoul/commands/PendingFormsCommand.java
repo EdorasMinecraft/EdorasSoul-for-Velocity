0fcfa5306aed17937f6cb1358577bc43f14e3d73
Kikisito <35898087+Kikisito@users.noreply.github.com>
2020-07-21 02:27:18 +0200

    Añadidos enlaces a formularios nuevos

--- src/main/java/com/github/kikisito/bungee/edorassoul/commands/PendingFormsCommand.java
+++ src/main/java/com/github/kikisito/bungee/edorassoul/commands/PendingFormsCommand.java
@@ -28,18 +28,22 @@ public class PendingFormsCommand extends Command {
         if(pendingForm.isPendingForms()){
             JsonArray jsonArray = pendingForm.getJsonArray();
             StringBuilder players = new StringBuilder();
+            StringBuilder playersmc = new StringBuilder();
             for(int i = 0; i < jsonArray.size(); i++){
-                players.append(jsonArray.get(i).getAsJsonObject().get("nick").getAsString());
-                if(i + 1 <= jsonArray.size() - 2) players.append(", ");
-                if(i + 1 == jsonArray.size() - 1) players.append(" y ");
+                String player = jsonArray.get(i).getAsJsonObject().get("nick").getAsString();
+                String link = "https://edoras.es/area51/formulario.php?usuario=" + jsonArray.get(i).getAsJsonObject().get("id").getAsString();
+                players.append("[" + player + "](" + link + ")");
+                playersmc.append(player);
+                if(i + 1 <= jsonArray.size() - 2){ players.append(", "); playersmc.append(", "); }
+                if(i + 1 == jsonArray.size() - 1){ players.append(" y "); playersmc.append(" y "); }
             }
             for(ProxiedPlayer p : plugin.getProxy().getPlayers()){
                 if(p.hasPermission("a51.notificaciones")){
-                    p.sendMessage(TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&', config.getString("formularios.new-form").replace("{players}", players.toString()))));
+                    p.sendMessage(TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&', config.getString("formularios.new-form").replace("{players}", playersmc))));
                 }
             }
             try {
-                Main.telegramBot.getTelegramBot().sendMessage(config.get("formularios.telegram-channel"), config.getString("formularios.telegram-new-form").replace("{players}", players.toString()), ParseMode.MARKDOWN, false, false, null, null);
+                Main.telegramBot.getTelegramBot().sendMessage(config.get("formularios.telegram-channel"), config.getString("formularios.telegram-new-form").replace("{players}", players), ParseMode.MARKDOWN, false, false, null, null);
             } catch (TelegramException e) {
                 e.printStackTrace();
             }
